b568095cef94630310b6ebeeea9b65efd61170ff
Bartosz Marcinkowski <bm371613@gmail.com>
2018-03-11 16:19:50 +0100

    added python_requires to setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     name='slice-aggregator',
     version='0.1',
     packages=find_packages(exclude=['tests']),
+    python_requires=">=3.5",
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
 )
