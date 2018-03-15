4f5f385fe3b718ace3a890b6f495ed6ea5dc812f
Bartosz Marcinkowski <bm371613@gmail.com>
2018-03-15 21:17:46 +0100

    filled setup.py

--- setup.py
+++ setup.py
@@ -12,9 +12,31 @@ with open(os.path.join(here, 'slice_aggregator', '__about__.py')) as f:
 with open(os.path.join(here, 'requirements', 'tests.txt')) as f:
     tests_require = list(f.read().split('\n'))
 
+with open(os.path.join(here, 'README.rst')) as f:
+    README = f.read()
+
 setup(
     name='slice-aggregator',
     version=about['__version__'],
+    description=
+    'A library for aggregating values assigned to indices by slices and the other way around ',
+    long_description=README,
+    url='https://github.com/bm371613/slice-aggregator',
+    author='Bartosz Marcinkowski',
+    author_email='bm371613@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    project_urls={
+        'Documentation': 'https://slice-aggregator.readthedocs.io/',
+        'Source': 'https://github.com/bm371613/slice-aggregator/',
+        'Tracker': 'https://github.com/bm371613/slice-aggregator/issues',
+    },
     packages=find_packages(exclude=['tests']),
     python_requires=">=3.5",
     setup_requires=['pytest-runner'],
